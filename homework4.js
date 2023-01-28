fb84ce8ebbcf28e31220d4f76e25a5469bfbfe42
Rivaldi Aditya <negararivaldi@gmail.com>
2023-01-28 23:46:30 +0700

    homework4

---
+++ homework4.js
@@ -0,0 +1,23 @@
+// let angkaAcak = Math.ceil (Math.random () * 50) 
+
+let angka 
+while (angka < 100) {
+    angka++;
+    console.log (Math.ceil(Math.random() * 50))
+}
+
+let angka1
+while (angka1 < 100) {
+  angka1++;
+  angka1 % 2 === 0;
+  console.log (Math.ceil(Math.random() * 50))
+}
+//if (angka % 2 === 0) {
+  //  console.log (angka + " " + "Bilangan Genap")
+//} else {
+//    console.log (angka + " " + "Bilangan Ganjil")
+//}
+
+//let array1 = [15, 3, 10, 42, 32, 34, 36, 21, 4, 3, 31, 28, 39, 40]
+
+//let array2 = []
