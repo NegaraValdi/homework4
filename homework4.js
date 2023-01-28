ffc131fb812f4684c110eed141c337e095d2c838
Rivaldi Aditya <negararivaldi@gmail.com>
2023-01-28 23:55:25 +0700

    homework4.js

--- homework4.js
+++ homework4.js
@@ -18,6 +18,13 @@ while (angka1 < 100) {
 //    console.log (angka + " " + "Bilangan Ganjil")
 //}
 
-//let array1 = [15, 3, 10, 42, 32, 34, 36, 21, 4, 3, 31, 28, 39, 40]
+let array1 = [14, 4, 10, 42, 32, 34, 36, 22, 4, 2, 36, 28, 40, 42]
 
-//let array2 = []
+let array2 = [5, 31, 3, 33, 29, 27, 15, 11, 7, 5, 25, 41, 53, 15]
+
+const genap = []
+
+const ganjil = []
+
+// maaf saya kurang memahami materi, karena minggu ini saya ada sidang skripsi
+// jadi saya belom mengikuti materi sepenuhnya. saya baru mengikuti materi pada jumat malam.
